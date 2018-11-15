5bea4f427d630e9be3922ff25bc13c40f9fd7415
davislf2.net@gmail.com <davislf2.net@gmail.com>
2018-11-15 13:27:02 +1100

    simple sklearn and joblib.dump

--- flask-sklearn/flask-sklearn.py
+++ flask-sklearn/flask-sklearn.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-"""Documentation about the flask-sklearn module."""
 
-import collections
-import os
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.externals import joblib
 
-# FIXME: put actual code here
-x = {'a': 13, 'b': 14}
+# Grab the dataset from scikit-learn
+X, y = datasets.load_iris(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42)
 
+# Build and train the model
+model = RandomForestClassifier(random_state=101)
+model.fit(X_train, y_train)
+print("Score on the training set is: {:2}".format(
+    model.score(X_train, y_train)))
+print("Score on the test set is: {:.2}".format(model.score(X_test, y_test)))
 
-class ClassA():
-    """
-    This is how we use it.
-    """
-
-
-def example():
-    print("Hello World!")
-
-
-example()
+# Save the model
+model_filename = 'iris-rf-v1.0.pkl'
+print("Saving model to {}...".format(model_filename))
+joblib.dump(model, model_filename)

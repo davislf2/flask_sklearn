8accd2e5144ab5df18d7d793c8dd14ec213a3343
davislf2.net@gmail.com <davislf2.net@gmail.com>
2018-11-15 12:52:58 +1100

    changed to python 3.6, tried all linting tools, and updated .gitignore

--- flask-sklearn/flask-sklearn.py
+++ flask-sklearn/flask-sklearn.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-"""Documentation about the flask-sklearn module."""
 
+import collections
+import os
 
 # FIXME: put actual code here
+x = {'a': 13, 'b': 14}
+
+
+class ClassA():
+    """
+    This is how we use it.
+    """
+
+
 def example():
     print("Hello World!")
 
